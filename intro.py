a2f1e7c7df8b48c99cc5411ba8f966738b757298
NivedhithaAN <86760148+NivedhithaAN@users.noreply.github.com>
2021-09-22 20:02:49 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,12 @@
+# intro
+print("Hello Gamer! What is your good name?")
+name = input()
+print("Hi " +name+ "!")
+print("~Lets start The Hangman Game~")
+print("Save the man with your luck,fight for it!")
+
+#words
+import random
+words = ['indonesia','Ocean','table','cottage','filament','basket']
+word = random.choice(words)
+print(word)
