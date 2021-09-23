e4f02c9d14f2d321c398dd52f74235ca8a12cc17
Fatima Ismail <90127260+Fatimah186@users.noreply.github.com>
2021-09-23 23:14:33 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,6 +2,9 @@
 print("Hello Gamer! What is your good name?")
 name = input()
 print("Hi " +name+ "!")
+print("******HERE ARE SOME RULES******")
+print("You've got 6 lives and each incorrect answer will cost a body part!!! ")
+print("Once all the 6 lives are used, the man will be hanged!!!")
 print("~Lets start The Hangman Game~")
 print("Save the man with your luck,fight for it!")
 
