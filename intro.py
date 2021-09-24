aa82c6f6a08f8255b3f70492baf01f12c15489d1
Fatima Ismail <90127260+Fatimah186@users.noreply.github.com>
2021-09-24 20:59:00 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -3,13 +3,17 @@ print("Hello Gamer! What is your good name?")
 name = input()
 print("Hi " +name+ "!")
 print("******HERE ARE SOME RULES******")
+print("----------------------------------------------------------------")
 print("You've got 6 lives and each incorrect answer will cost a body part!!! ")
 print("Once all the 6 lives are used, the man will be hanged!!!")
+print("----------------------------------------------------------------")
 print("~Lets start The Hangman Game~")
 print("Save the man with your luck,fight for it!")
+print("----------------------------------------------------------------")
 
 #words
 import random
-words = ['indonesia','Ocean','table','cottage','filament','basket']
-word = random.choice(words)
-print(word)
+
+from words import words
+
+print("The random word is " +random.choice(words))
